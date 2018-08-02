5b82932ec3b7f0e018ab41c89a279b52cd66e172
SahilThakur7 <sahilrd07@gmail.com>
2018-08-02 10:28:18 +0530

    error modal implemented

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux';
 
-const withErrorHandler = (WrappedComponent) => {
-    return(props) => {
-        return (
-            <Aux>
-                <Modal show>
-                        Not working yet.
-                </Modal>
-                    <WrappedComponent {...props} />
-            </Aux>
-        )
+const withErrorHandler = (WrappedComponent , axios) => {
+    return class extends Component {
+
+        state = {
+            error: null
+        }
+
+        componentDidMount(){
+            axios.interceptors.request.use(req => {
+                this.setState({
+                    error: null
+                });
+                return req;
+            });
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({
+                    error: error
+                });
+            });
+        }
+
+        confirmedHandler = () => {
+            this.setState({
+                error: null
+            })
+        }
+
+        render(){
+            return(
+
+                <Aux>
+                    <Modal show={this.state.error}
+                     modalClosed={this.confirmedHandler}
+                    >
+                        {this.state.error? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            )
+        }
     }
 }
 
