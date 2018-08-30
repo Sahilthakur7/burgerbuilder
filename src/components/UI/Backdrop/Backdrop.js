f9a4215590032d25a5f8eb4ba46709bce812b088
SahilThakur7 <sahilrd07@gmail.com>
2018-08-30 10:29:02 +0530

    started with the form

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,13 +2,7 @@ import React from 'react';
 import classes from './Backdrop.css';
 
 const backdrop = (props) => (
-    console.log(props);
-    return(
-        <div>
-            Hello
-        </div>
-    )
-    // props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
+    props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
 );
 
 export default backdrop;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,18 +3,62 @@ import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component{
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
-        },
-        loading: false
+        orderForm:{
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your zipCode'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'email',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [{value: 'fastest',displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}]
+                },
+                value: ''
+            },
+            loading: false
+        }
     }
-
     orderHandler = (event) => {
         event.preventDefault();
         const order = {
@@ -48,10 +92,10 @@ class ContactData extends Component{
     render(){
         let form = (
             <form >
-                <input type="text" name="name" placeholder="Your name"/>
-                <input type="email" name="email" placeholder="Your email"/>
-                <input type="text" name="street" placeholder="Your street"/>
-                <input type="text" name="postal" placeholder="Your postal code"/>
+                <Input elementType="..." elementConfig="..." value="..."/>
+                <Input inputtype="input" type="email" name="email" placeholder="Your email"/>
+                <Input inputtype="input" type="text" name="street" placeholder="Your street"/>
+                <Input inputtype="input" type="text" name="postal" placeholder="Your postal code"/>
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
